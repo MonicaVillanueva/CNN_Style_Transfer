da91b3e46a7eeeb5d1a93b306fd2c32b5f0568b4
MonicaVillanueva <monicavi@kth.se>
2017-05-12 15:38:52 +0200

    Small test - 10 epochs

--- style_transfer/style_transfer.py
+++ style_transfer/style_transfer.py
@@ -2,18 +2,26 @@ import tensorflow as tf
 import os
 from VGG import vgg16 as net
 import utils
+import numpy as np
 import matplotlib.pyplot as plt
 
 
 ## Constants
+VGG16_MEANS = [123.68, 116.779, 103.939]
 PIC_SIZE = 224
 CHANNELS = 3
 ALPHA = 1
 BETA = 100
 
+INI_ETA = 1.5
+DECAY = 0.95
+STEPS = 500
+N_EPOCHS = 10
+
 ## Paths
 model_path = os.path.join('VGG', 'vgg16_weights.npz')
 original_images_path = os.path.join('Dataset', 'original_input')
+output_path = os.path.join(os.getcwd(), 'Test')
 photo_path = os.path.join(original_images_path, 'neckarfront.jpg')
 paint_path = os.path.join(original_images_path, 'van_gogh.jpg')
 
@@ -26,12 +34,13 @@ vgg = net.vgg16(imgs, model_path, sess)
 
 
 ## Images
-photo = utils.read_image(photo_path, PIC_SIZE, PIC_SIZE)
-paint = utils.read_image(paint_path, PIC_SIZE, PIC_SIZE)
+photo = utils.read_image(photo_path, PIC_SIZE, PIC_SIZE, VGG16_MEANS)
+paint = utils.read_image(paint_path, PIC_SIZE, PIC_SIZE, VGG16_MEANS)
 # Create white noise image
 mean = 0
 std = 1
-white_noise = utils.create_white_noise(mean, std, PIC_SIZE, PIC_SIZE)
+white_noise_img = utils.create_white_noise(mean, std, PIC_SIZE, PIC_SIZE, VGG16_MEANS)  # TODO: test with original mixed with noise?
+white_noise = tf.Variable(tf.constant(np.array(white_noise_img, dtype=np.float32)), trainable=True, name='white_noise')
 # plt.imshow(white_noise)
 
 
@@ -40,10 +49,10 @@ print("Get responses")
 responses = [vgg.conv1_1, vgg.conv2_1, vgg.conv3_1, vgg.conv4_1, vgg.conv5_1]
 content_responses = [sess.run(res, feed_dict={vgg.imgs: [photo]}) for res in responses]
 style_responses = [sess.run(res, feed_dict={vgg.imgs: [paint]}) for res in responses]
-noise_responses = [sess.run(res, feed_dict={vgg.imgs: [white_noise]}) for res in responses]
+noise_responses = [sess.run(res, feed_dict={vgg.imgs: [white_noise_img]}) for res in responses]
 
 
-print("Compute Gram Matrix")# TODO: generalize to use any number of layers
+print("Compute Gram Matrix")    # TODO: generalize to use any number of layers
 style_gram_matrix = utils.compute_gram_matrix(sess, style_responses)
 noise_gram_matrix = utils.compute_gram_matrix(sess, noise_responses)
 
@@ -55,8 +64,34 @@ print("Train")
 cont_loss = utils.compute_content_loss(sess, content_responses, noise_responses)
 style_loss = utils.compute_style_loss(sess, style_responses, style_gram_matrix, noise_gram_matrix)
 
-
 # Compute total loss (alpha * content_loss + beta * style_loss)
-loss = ALPHA * cont_loss + BETA * style_loss
+loss = tf.Variable(ALPHA * cont_loss + BETA * style_loss, dtype=tf.float64)
+# loss = ALPHA * cont_loss + BETA * style_loss
+
+# Create tf optimizer
+global_step = tf.Variable(0, trainable=False)
+eta = tf.train.exponential_decay(learning_rate=INI_ETA, global_step=global_step, decay_steps=STEPS, decay_rate=DECAY, staircase=False)
+train_step = tf.train.AdamOptimizer(eta).minimize(loss, global_step=global_step)
+# optimizer = tf.train.AdamOptimizer(INI_ETA)   # No decay
+# train_step = optimizer.minimize(loss)
+
+
+## Optimize
+print("Optimize")
+
+sess.run(tf.global_variables_initializer())
+for e in range(N_EPOCHS):
+
+    # Feedback
+    if e % 50 == 0:
+        print('Epoch: ', e)
+        print('Loss: ', sess.run(loss))
+        img = sess.run(white_noise)
+        utils.save_image(img, VGG16_MEANS, e, output_path)
+
+    # Keep training
+    print('Epoch: ', e)
+    print('Loss: ', sess.run(loss))
+    sess.run(train_step)
 
 
--- style_transfer/utils.py
+++ style_transfer/utils.py
@@ -1,13 +1,41 @@
 import tensorflow as tf
-from scipy.misc import imread, imresize
+from scipy.misc import imread, imresize, imsave
 import numpy as np
+import os
+import matplotlib.pyplot as plt
 
 
-def read_image(path, height, width):
+def read_image(path, height, width, sub_mean):
     img = imread(path, mode='RGB')
     img = imresize(img, (height, width))
+    return img - sub_mean
 
-    return img
+
+def save_image(img, mean, epoch, out_path=os.getcwd()):
+    # If dir does not exit
+    if not os.path.exists(out_path):
+        os.makedirs(out_path)
+
+    # Path
+    fname = 'result_epoch', epoch, '.jpg'
+    fname = ''.join(map(str, fname))
+    if out_path != os.getcwd():
+        path = os.path.join(out_path, fname)
+    else:
+        path = fname
+
+    # Image
+    img_save = img + mean
+    # img_save = np.clip(img_save, 0, 255).astype('uint8')
+
+    # Save
+    imsave(path, img_save)
+
+
+
+def create_white_noise(mean, std, height, width, sub_mean):
+    img = np.random.normal(mean, std, size=(height, width, 3)).astype('float32')
+    return img - sub_mean
 
 
 def compute_gram_matrix(sess, responses):
@@ -29,10 +57,6 @@ def compute_gram_matrix(sess, responses):
     return gram_matrix
 
 
-def create_white_noise(mean, std, height, width):
-    return np.random.normal(mean, std, size=(height, width, 3)).astype('float32')
-
-
 def compute_content_loss(sess, resp_orig, resp_noise):
     """
     Equation 1 from paper
@@ -41,7 +65,7 @@ def compute_content_loss(sess, resp_orig, resp_noise):
     for layer in range(len(resp_orig)):
         loss_lay = 0.5 * tf.reduce_sum(tf.pow(resp_orig[layer] - resp_noise[layer], 2))
         loss.append(sess.run(loss_lay))
-    return loss
+    return sum(loss)
 
 
 def compute_style_loss(sess, feat_maps, gram_orig, gram_noise):
@@ -49,12 +73,13 @@ def compute_style_loss(sess, feat_maps, gram_orig, gram_noise):
     Equation 4 and 5 from paper
     """
 
-    contributions = []
+    loss = 0
     for layer in range(len(feat_maps)):
         N = feat_maps[layer].shape[3]                                   # distinct filters
         M = feat_maps[layer].shape[1] * feat_maps[layer].shape[2]       # size of each feature map
 
-        contributions.append(sess.run((1 / (4 * N ** 2 * M ** 2)) * tf.reduce_sum(tf.pow(gram_orig[layer] - gram_noise[layer], 2))))
+        contribution = sess.run((1 / (4 * N ** 2 * M ** 2)) * tf.reduce_sum(tf.pow(gram_orig[layer] - gram_noise[layer], 2)))
+        style_W = 1. / len(feat_maps)
+        loss = loss + style_W * contribution
 
-    loss = tf.reduce_sum(style_W * contributions) #TODO
-    return sess.run(loss)
+    return loss
